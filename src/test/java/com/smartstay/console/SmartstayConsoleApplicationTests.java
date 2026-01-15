7dea86ee4da23308deb5db0c16dae0cc17224e84
Sujith <techrules07@gmail.com>
2026-01-15 11:48:20 +0530

    initial push

---
+++ src/test/java/com/smartstay/console/SmartstayConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartstay.console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstayConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
