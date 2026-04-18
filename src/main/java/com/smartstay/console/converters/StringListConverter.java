f9b9edd0acf6102d4d7e7fffcddf8db03aea1c23
ArunS3Remotica <arun.t@s3remotica.com>
2026-04-18 17:17:48 +0530

    Set converter for billing rules reminder days issue

--- src/main/java/com/smartstay/console/converters/StringListConverter.java
+++ src/main/java/com/smartstay/console/converters/StringListConverter.java
@@ -3,11 +3,15 @@ package com.smartstay.console.converters;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
 
 import java.util.List;
 
+@Converter
 public class StringListConverter implements AttributeConverter<List<String>, String> {
+
     private static final ObjectMapper mapper = new ObjectMapper();
+
     @Override
     public String convertToDatabaseColumn(List<String> attribute) {
         try {
