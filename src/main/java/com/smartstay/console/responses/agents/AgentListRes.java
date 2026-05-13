2041197385d3551173da7230bd470778d3326d06
ArunS3Remotica <arun.t@s3remotica.com>
2026-05-13 13:41:08 +0530

    get agents pagination and roles dropdown api added

--- src/main/java/com/smartstay/console/responses/agents/AgentListRes.java
+++
@@ -1,7 +0,0 @@
-package com.smartstay.console.responses.agents;
-
-import java.util.List;
-
-public record AgentListRes(List<AgentResponse> activeAgents,
-                           List<AgentResponse> inActiveAgents) {
-}
