c01361895af7efb807e04baad3b7265b9e4eeacb
ArunS3Remotica <arun.t@s3remotica.com>
2026-05-19 12:50:21 +0530

    paymentUrl in orderHistory response, showFlatCharge in ebConfig response, canVerifyPayment now checks for agent user type.

--- src/main/java/com/smartstay/console/responses/hostels/EbConfig.java
+++ src/main/java/com/smartstay/console/responses/hostels/EbConfig.java
@@ -3,6 +3,7 @@ package com.smartstay.console.responses.hostels;
 public record EbConfig(Integer ebConfigId,
                        boolean shouldIncludeInRent,
                        String typeOfReading,
+                       boolean showFlatCharge,
                        Double charge,
                        Double flatCharge,
                        Integer billDate,
