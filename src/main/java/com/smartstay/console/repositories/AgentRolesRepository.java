647f8326cd14b52fd3177f1ae4b9dfc9faa9c782
ArunS3Remotica <arun.t@s3remotica.com>
2026-05-29 15:09:29 +0530

    Role add, delete now doesnt check isDeleted roles

--- src/main/java/com/smartstay/console/repositories/AgentRolesRepository.java
+++ src/main/java/com/smartstay/console/repositories/AgentRolesRepository.java
@@ -14,18 +14,22 @@ public interface AgentRolesRepository extends JpaRepository<AgentRoles, Long> {
     AgentRoles findByRoleId(Long roleId);
 
     @Query(value = """
-    SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
-    FROM agent_roles role
-    WHERE role.role_name = :roleName
-    """, nativeQuery = true)
+            SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
+            FROM agent_roles role
+            WHERE role.role_name = :roleName
+                and role.is_deleted = false
+            """, nativeQuery = true)
     int existsByRoleName(@Param("roleName") String roleName);
 
     @Query(value = """
-    SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
-    FROM agent_roles role
-    WHERE role.role_name = :roleName and role.role_id != :roleId
-    """, nativeQuery = true)
-    int existsByRoleNameNotRoleId(@Param("roleName") String roleName,@Param("roleId") long roleId);
+            SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END
+            FROM agent_roles role
+            WHERE role.role_name = :roleName
+                and role.role_id != :roleId
+                and role.is_deleted = false
+            """, nativeQuery = true)
+    int existsByRoleNameNotRoleId(@Param("roleName") String roleName,
+                                  @Param("roleId") long roleId);
 
     List<AgentRoles> findAllByIsActiveTrueAndIsDeletedFalse();
 
