823a93016cf4ffb2f16f986f43c87326ab79680e
ArunS3Remotica <arun.t@s3remotica.com>
2026-06-15 22:27:10 +0530

    merge fix

--- src/main/java/com/smartstay/console/repositories/DemoRequestActivityRepository.java
+++ src/main/java/com/smartstay/console/repositories/DemoRequestActivityRepository.java
@@ -10,18 +10,7 @@ import java.util.Set;
 @Repository
 public interface DemoRequestActivityRepository extends JpaRepository<DemoRequestActivity, Long> {
 
-    @Query("""
-            select count(a)
-            from DemoRequestActivity a
-            where a.createdAt >= :startDate and a.createdAt < :endDate
-                and a.status = :status
-            """)
-    long getStatusCount(@Param("startDate") Date startDate,
-                        @Param("endDate") Date endDate,
-                        @Param("status") String status);
-
     List<DemoRequestActivity> findAllByRequestIdInOrderByActivityIdDesc(Set<Long> demoRequestIds);
-    List<DemoRequestActivity> findAllByRequestIdIn(Set<Long> demoRequestIds);
 
     List<DemoRequestActivity> findAllByRequestIdOrderByActivityIdDesc(Long requestId);
 }
