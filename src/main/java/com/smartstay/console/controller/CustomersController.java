a09d72f313be61528c1ab4e87cdcbebb5ffc717b
ArunS3Remotica <arun.t@s3remotica.com>
2026-07-10 16:03:36 +0530

    settlement info endpoint change

--- src/main/java/com/smartstay/console/controller/CustomersController.java
+++ src/main/java/com/smartstay/console/controller/CustomersController.java
@@ -58,7 +58,7 @@ public class CustomersController {
         return customersService.getCustomersWithPendingAdvanceDeductions();
     }
 
-    @GetMapping("/settlement-info/{customerId}")
+    @GetMapping("/settlement/{customerId}")
     public ResponseEntity<?> getCustomerSettlementInfo(@PathVariable("customerId") String customerId,
                                                        @Valid @RequestBody CustomerDatePayload payload){
         return customersService.getCustomerSettlementInfo(customerId, payload);
