a68fb872540fbbc055a30c7796ea44bafbac6e22
ArunS3Remotica <arun.t@s3remotica.com>
2026-07-15 12:58:55 +0530

    generate payment link api now accepts 0 discount amount

--- src/main/java/com/smartstay/console/payloads/orderHistory/PaymentLinkGeneratePayload.java
+++ src/main/java/com/smartstay/console/payloads/orderHistory/PaymentLinkGeneratePayload.java
@@ -2,12 +2,12 @@ package com.smartstay.console.payloads.orderHistory;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record PaymentLinkGeneratePayload(@NotBlank(message = "Plan code is required")
                                          String planCode,
                                          @NotNull(message = "Discount amount is required")
-                                         @Positive(message = "Discount amount must be greater than 0")
+                                         @PositiveOrZero(message = "Discount amount must not be less than 0")
                                          Double discountAmount,
                                          @NotBlank(message = "Paid by is required")
                                          String paidBy) {
