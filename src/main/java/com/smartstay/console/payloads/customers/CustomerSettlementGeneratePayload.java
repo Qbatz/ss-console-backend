b22e05eaeddf90881d8f2d01711302fe966b0631
ArunS3Remotica <arun.t@s3remotica.com>
2026-07-29 13:24:47 +0530

    validation for leaving date

--- src/main/java/com/smartstay/console/payloads/customers/CustomerSettlementGeneratePayload.java
+++ src/main/java/com/smartstay/console/payloads/customers/CustomerSettlementGeneratePayload.java
@@ -1,12 +1,14 @@
 package com.smartstay.console.payloads.customers;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record CustomerSettlementGeneratePayload(@JsonFormat(pattern = "dd-MM-yyyy")
+public record CustomerSettlementGeneratePayload(@NotNull(message = "Leaving date is required")
+                                                @JsonFormat(pattern = "dd-MM-yyyy")
                                                 LocalDate leavingDate,
                                                 boolean isCustomRent,
                                                 @PositiveOrZero(message = "Custom rent amount must not be less than 0")
